aee309ad8f6de92e0e72ebaaf4a4300626c18691
GTPSHAX <rafiehasannudin@gmail.com>
2025-04-18 15:06:23 -0700

    fix(Server): update SSL certificates and add chain file for osm.growplus.asia
    Updated the SSL certificates for the domain osm.growplus.asia and added the chain file to ensure proper SSL handshake. Also improved request logging by adding client IP and error handling for logging failures.

--- src/Server.js
+++ src/Server.js
@@ -4,25 +4,30 @@ const https = require('https');
 const fs = require('fs');
 const tls = require('tls');
 
-const sniDefaultCert = fs.readFileSync(path.join(__dirname, '..', 'certs', 'cdngtps.my.id', '_.cdngtps.my.id-crt.pem'));
-const sniDefaultKey = fs.readFileSync(path.join(__dirname, '..', 'certs', 'cdngtps.my.id', '_.cdngtps.my.id-key.pem'));
+const sniDefaultCert = fs.readFileSync(path.join(__dirname, '..', 'certs', 'osm.growplus.asia', 'osm.growplus.asia-crt.pem'));
+const sniDefaultKey = fs.readFileSync(path.join(__dirname, '..', 'certs', 'osm.growplus.asia', 'osm.growplus.asia-key.pem'));
+const sniDefaultChain = fs.readFileSync(path.join(__dirname, '..', 'certs', 'osm.growplus.asia', 'osm.growplus.asia-chain.pem'));
 
 const sniCallback = (serverName, callback) => {
     console.log(`Requested SNI for: ${serverName}`);
     let cert = sniDefaultCert;
     let key = sniDefaultKey;
+    let ca = sniDefaultChain;
 
     if (serverName === 'www.growtopia1.com' || serverName === 'www.growtopia2.com') {
         cert = fs.readFileSync(path.join(__dirname, '..', 'certs', 'growtopia1.com', 'gt-crt.pem'));
         key = fs.readFileSync(path.join(__dirname, '..', 'certs', 'growtopia1.com', 'gt-key.pem'));
+        ca = null;
+        passphrase = null;
     }
 
-    callback(null, tls.createSecureContext({ cert, key }));
+    callback(null, tls.createSecureContext({ cert, key, ca }));
 }
 
 const serverOptions = {
     cert: sniDefaultCert,
     key: sniDefaultKey,
+    ca: sniDefaultChain,
     SNICallback: sniCallback,
     keepAliveTimeout: 60000, // 60 seconds
     headersTimeout: 65000, // 65 seconds
@@ -32,11 +37,15 @@ const serverOptions = {
 app.listen(80, () => {
     console.log('Server started at http://localhost:80');
 }).on('request', (req, res) => {
-	const currentTime = new Date().toISOString();
-    const clientIP = req.headers['x-forwarded-for']?.split(',')[0] || req.socket.remoteAddress || req.ip;
-    console.log(
-        `[HTTP][${req.get('host')}] ${req.method} -> ${req.originalUrl} - ${currentTime}`,
-    );
+	try {
+        const currentTime = new Date().toISOString();
+        const clientIP = req.headers['x-forwarded-for']?.split(',')[0] || req.socket.remoteAddress || req.ip;
+        console.log(
+            `[HTTP][${req.get('host')}][${clientIP}] ${req.method} -> ${req.originalUrl} - ${currentTime}`,
+        );
+    } catch (error) {
+        console.error('Error logging request:', error);
+    }
 }).on('tlsClientError', (err, socket) => {
     console.error('Error handshake TLS:', err.message);
     socket.destroy();
@@ -49,11 +58,15 @@ app.listen(80, () => {
 https.createServer(serverOptions, app).listen(443, () => {
     console.log('Secure server started at https://localhost:443');
 }).on('request', (req, res) => {
-	const currentTime = new Date().toISOString();
-    const clientIP = req.headers['x-forwarded-for']?.split(',')[0] || req.socket.remoteAddress || req.ip;
-    console.log(
-        `[HTTPS][${req.get('host')}] ${req.method} -> ${req.originalUrl} - ${currentTime}`,
-    );
+	try {
+        const currentTime = new Date().toISOString();
+        const clientIP = req.headers['x-forwarded-for']?.split(',')[0] || req.socket.remoteAddress || req.ip;
+        console.log(
+            `[HTTPS][${req.get('host')}][${clientIP}] ${req.method} -> ${req.originalUrl} - ${currentTime}`,
+        );
+    } catch (error) {
+        console.error('Error logging request:', error);
+    }
 }).on('tlsClientError', (err, socket) => {
     console.error('Error handshake TLS:', err.message);
     socket.destroy();
