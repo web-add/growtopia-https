4cf2d6a92f3f85e558bdf9b7ac747f053a41d4fe
WebAdd <94047957+web-add@users.noreply.github.com>
2025-06-18 09:31:21 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,7 +2,7 @@ module.exports = {
     // your server name
     server_name: "Growtopia",
     // public ipv4
-    server_ip: "127.0.0.1",
+    server_ip: "26.155.188.111",
     // UDP port
     server_port: 17091,
     // login url dashboard
